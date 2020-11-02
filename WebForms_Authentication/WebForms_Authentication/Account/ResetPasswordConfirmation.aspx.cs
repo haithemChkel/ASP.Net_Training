f4bcf84b26be5100fc262f82a479dbac5fe375e9
haithem khalifa <v-haikha@microsoft.com>
2020-11-02 19:59:29 +0100

    Add Athentication sample

---
+++ WebForms_Authentication/WebForms_Authentication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebForms_Authentication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebForms_Authentication/WebForms_Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebForms_Authentication.Global" Language="C#" %>
